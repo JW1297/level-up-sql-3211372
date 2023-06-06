9e20bf0a4dea615abbff82d066ddbd8d80ebc5ae
JW1297 <114872686+JW1297@users.noreply.github.com>
2023-06-06 19:24:43 +0000

    First_Commit

--- 01_restaurant/challenges/01_03_printmenus_ch.sql
+++ 01_restaurant/challenges/01_03_printmenus_ch.sql
@@ -3,3 +3,15 @@
 -- Create a report with all the items sorted by price (lowest to highest).
 -- Create a report showing appetizers and beverages.
 -- Create a report with all items except beverages.
+
+-- Report 1
+-- SELECT Name, Price FROM Dishes
+-- ORDER BY Price ASC;
+
+-- Report 2
+-- SELECT Name, Type FROM Dishes
+-- WHERE Type = 'Appetizer' OR Type = 'Beverage';
+
+-- Report 3
+-- SELECT * FROM Dishes
+-- WHERE TYPE != 'Beverage';
---
+++ 01_restaurant/challenges/01_04_deleteduplicate_ch.sql
@@ -0,0 +1,3 @@
+DELETE FROM Customers WHERE CustomerID = 102;
+
+SELECT * FROM Customers ORDER BY CustomerID DESC;
