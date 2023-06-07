bf698e9c897800791abe6a2f0f6ead5aba51ffbd
JW1297 <114872686+JW1297@users.noreply.github.com>
2023-06-07 12:06:11 +0000

    remove_record_done

--- 01_restaurant/challenges/01_06_removeinfo_ch.sql
+++ 01_restaurant/challenges/01_06_removeinfo_ch.sql
@@ -1,5 +1,17 @@
 -- Remove incorrect information from the database.
 
--- A customer named Norby has notified us he won't 
--- be able to keep his Friday reservation. 
--- Today is July 24, 2022.
+-- A customer named Maggi has notified us she won't 
+-- be able to keep her reservation on 2022-05-11 20:00:00. 
+
+-- The Query
+SELECT * FROM Reservations 
+LEFT JOIN Customers 
+ON Reservations.CustomerID = Customers.CustomerID
+WHERE Customers.FirstName = 'Maggi' AND Date = '2022-05-11 20:00:00';
+
+-- The Delete
+DELETE FROM Reservations
+WHERE ReservationID = 1943;
+
+-- I was hoping for a more integrated solution 
+-- rather than calling the queried ID
